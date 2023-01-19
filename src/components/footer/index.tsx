b3acd0728732dfed211813ec543e79ec7f5af87f
TatsuKenshi <rockinbg@gmail.com>
2023-01-19 18:46:02 +0100

    adds footer w/basic styling

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        paddingTop: "2rem",
+        paddingBottom: "2rem",
+        backgroundColor: "skyblue",
+      }}
+    >
+      <h5 style={{ marginRight: "1rem" }}>&copy; {new Date().getFullYear()}</h5>
+      <span style={{ marginRight: "1rem" }}>ComfySloth</span>
+      <h5>All Rights Reserved</h5>
+    </div>
+  );
 };
 
 export default Footer;
