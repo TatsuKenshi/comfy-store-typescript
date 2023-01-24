46210a70a3f2dc0a3c9e9ba90686f0e7e775eed6
TatsuKenshi <rockinbg@gmail.com>
2023-01-24 22:55:44 +0100

    fixes the user and cart slice permutation

--- src/app/reducers/cartSlice.ts
+++ src/app/reducers/cartSlice.ts
@@ -2,11 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 import { RootStateType } from "../store";
 
 type InitialState = {
-  myUser: {} | null;
+  // cart: getLocalStorage(),
+  total_items: number;
+  total_amount: number;
+  shipping_fee: number;
 };
 
 const initialState: InitialState = {
-  myUser: null,
+  // cart: getLocalStorage(),
+  total_items: 0,
+  total_amount: 0,
+  shipping_fee: 534,
 };
 
 const cartSlice = createSlice({
--- src/app/reducers/userSlice.ts
+++ src/app/reducers/userSlice.ts
@@ -2,17 +2,11 @@ import { createSlice } from "@reduxjs/toolkit";
 import { RootStateType } from "../store";
 
 type InitialState = {
-  // cart: getLocalStorage(),
-  total_items: number;
-  total_amount: number;
-  shipping_fee: number;
+  myUser: {} | null;
 };
 
 const initialState: InitialState = {
-  // cart: getLocalStorage(),
-  total_items: 0,
-  total_amount: 0,
-  shipping_fee: 534,
+  myUser: null,
 };
 
 const cartSlice = createSlice({
