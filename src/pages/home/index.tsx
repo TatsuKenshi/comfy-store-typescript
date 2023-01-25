409b00757f7b25680e0bd7fb1f34460a8c024155
TatsuKenshi <rockinbg@gmail.com>
2023-01-25 19:37:13 +0100

    adds home page components

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import ContactSection from "../../components/contact";
+import FeaturedProducts from "../../components/featured-products";
+import HomeHero from "../../components/home-hero";
+import Mission from "../../components/mission";
 
 const Home = () => {
-  return <div>This is the home page.</div>;
+  return (
+    <div>
+      This is the home page.
+      <HomeHero />
+      <FeaturedProducts />
+      <Mission />
+      <ContactSection />
+    </div>
+  );
 };
 
 export default Home;
