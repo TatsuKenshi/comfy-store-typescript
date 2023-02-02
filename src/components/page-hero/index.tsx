9a9e26350db690a6dffac93f8e5ee9e265e330ef
TatsuKenshi <rockinbg@gmail.com>
2023-02-02 23:52:01 +0100

    adds the about page and the PageHero component

---
+++ src/components/page-hero/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// correct the prop type here
+const PageHero = (title: string | any) => {
+  return <div>PageHero</div>;
+};
+
+export default PageHero;
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,8 +1,34 @@
 import React from "react";
+import hero from "../../assets/hero-bcg.jpeg";
+import PageHero from "../../components/page-hero";
 import "./index.scss";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <main className="">
+      <PageHero title="About" />
+      <section className="">
+        <div className="">
+          <img src={hero} alt="hero" />
+        </div>
+        <article className="">
+          <div className="">
+            <h2 className="">Our Story</h2>
+            <div className=""></div>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+              possimus corporis quos fugiat animi quasi, accusamus
+              exercitationem ullam veniam commodi quisquam neque numquam.
+              Exercitationem inventore officiis quaerat eos enim accusamus
+              minima tenetur architecto quas, corporis nemo labore id a
+              provident libero reiciendis omnis placeat. Earum quibusdam
+              doloremque totam consequatur quis!
+            </p>
+          </div>
+        </article>
+      </section>
+    </main>
+  );
 };
 
 export default About;
