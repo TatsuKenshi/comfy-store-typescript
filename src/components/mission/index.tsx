af1c3525cb2414c7699ec8c397b87c5c70a3d949
TatsuKenshi <rockinbg@gmail.com>
2023-02-10 21:50:55 +0100

    adds the services/mission section to the home page

--- src/components/mission/index.tsx
+++ src/components/mission/index.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { services } from "../../utils/constants";
 
 const Mission = () => {
-  return <div>Mission</div>;
+  return (
+    <section>
+      <div className="section-center">
+        <article className="header">
+          <h3>
+            Custom Furniture <br />
+            Built Only For You
+          </h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero
+            obcaecati deserunt temporibus. Totam optio commodi minus vero sit
+            delectus atque.
+          </p>
+        </article>
+        <div className="services-center">
+          {services.map((service) => {
+            const { id, icon: Icon, title, text } = service;
+
+            return (
+              <article key={id} className="service">
+                <span className="icon">
+                  <Icon />
+                </span>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Mission;
