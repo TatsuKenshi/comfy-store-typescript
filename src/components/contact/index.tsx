4a96fc6d689906290573d1ec228d3c8b6ec3510a
TatsuKenshi <rockinbg@gmail.com>
2023-02-11 21:08:56 +0100

    adds the contact us section to the home page

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const ContactSection = () => {
-  return <div>ContactSection</div>;
+  return (
+    <section className="">
+      <article className="">
+        <h3 className="">
+          Custom Shop <br /> Built Only For You
+        </h3>
+        <p className="">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+          repellendus! Minus omnis, optio facere nam iure tempora nesciunt?
+          Voluptates, voluptatum quas nam ut sequi iure harum modi totam illo
+          magnam.
+        </p>
+      </article>
+      <div className="">
+        <button>
+          <Link to="/contact" className="">
+            Contact Us
+          </Link>
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default ContactSection;
