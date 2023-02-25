60fb535a32e22254fd2577001f8a079e2d1644ea
TatsuKenshi <rockinbg@gmail.com>
2023-02-25 21:49:07 +0100

    adds basic cart item UI setup (no styling, no functionality)

--- src/components/cart-columns/index.tsx
+++ src/components/cart-columns/index.tsx
@@ -2,19 +2,19 @@ const CartColumns = () => {
   return (
     <section>
       <div className="content" style={{ display: "flex" }}>
-        <h5 className="" style={{ marginRight: "10px" }}>
+        <h5 className="" style={{ width: "150px" }}>
           item
         </h5>
-        <h5 className="" style={{ marginRight: "10px" }}>
+        <h5 className="" style={{ width: "150px" }}>
           price
         </h5>
-        <h5 className="" style={{ marginRight: "10px" }}>
+        <h5 className="" style={{ width: "150px" }}>
           quantity
         </h5>
-        <h5 className="" style={{ marginRight: "10px" }}>
+        <h5 className="" style={{ width: "150px" }}>
           subtotal
         </h5>
-        <span className="" style={{ marginRight: "10px" }}>
+        <span className="" style={{ width: "150px" }}>
           remove
         </span>
       </div>
--- src/components/cart-item/index.tsx
+++ src/components/cart-item/index.tsx
@@ -1,7 +1,92 @@
 import { CartProductType } from "../../app/types";
+import { formatPrice } from "../../utils/helpers";
+import { FaTrash } from "react-icons/fa";
+import AmountButtons from "../amount-buttons/index";
+import { removeItem, toggleAmount } from "../../app/reducers/cartSlice";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../app/store";
 
-const CartItem = (product: CartProductType) => {
-  return <div>CartItem</div>;
+const CartItem = (item: CartProductType) => {
+  const { id, image, name, color, price, amount } = item;
+  const dispatch = useDispatch<AppDispatch>();
+
+  // increase and decrease amount functions
+  const increaseAmount = () => {
+    dispatch(toggleAmount({ id, toggleType: "increase" }));
+  };
+
+  const decreaseAmount = () => {
+    dispatch(toggleAmount({ id, toggleType: "decrease" }));
+  };
+
+  return (
+    <div className="" style={{ display: "flex", padding: "5px 0" }}>
+      {/* item */}
+      <div className="" style={{ width: "150px", display: "flex" }}>
+        <div className="">
+          <img src={image} alt={name} className="" width="50px" />
+        </div>
+
+        <div>
+          <div className="">
+            <h5 className="">{name}</h5>
+          </div>
+
+          <div className="">
+            <div className="">color : </div>
+            <div
+              className=""
+              style={{
+                marginRight: "5px",
+                borderRadius: "50%",
+                width: "20px",
+                height: "20px",
+                backgroundColor: color,
+                border: "none",
+                color: "white",
+              }}
+            ></div>
+          </div>
+
+          <div className="">
+            {/* this price is visible only on small screens */}
+            <p className="">{formatPrice(price)}</p>
+          </div>
+        </div>
+      </div>
+      {/* end of item */}
+
+      {/* price */}
+      <div className="" style={{ width: "150px" }}>
+        <p className="">{formatPrice(price)}</p>
+      </div>
+      {/* end of price */}
+
+      {/* amount buttons */}
+      <div className="" style={{ width: "150px" }}>
+        <AmountButtons
+          amount={amount}
+          increaseAmount={increaseAmount}
+          decreaseAmount={decreaseAmount}
+        />
+      </div>
+      {/* end of amount buttons */}
+
+      {/* subtotal */}
+      <div className="" style={{ width: "150px" }}>
+        <h5 className="">{formatPrice(price * amount)}</h5>
+      </div>
+      {/* end of subtotal */}
+
+      {/* remove button */}
+      <div className="" style={{ width: "150px" }}>
+        <button className="" onClick={() => removeItem(id)}>
+          <FaTrash className="" />
+        </button>
+      </div>
+      {/* end of remove button */}
+    </div>
+  );
 };
 
 export default CartItem;
