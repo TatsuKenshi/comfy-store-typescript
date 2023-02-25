9488db6e7af1b251c94940eed5680e9a2ba5a578
TatsuKenshi <rockinbg@gmail.com>
2023-02-25 23:30:28 +0100

    adds basic Auth0 setup - Auth0Provider and necessary props

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 
 // application wrappers go here, including the theming provider, i18next provider, etc
+
+// auth domain dev-n7f-c1rx.us.auth0.com
+// auth clientid Kdv0f1TxqUbSMGqU4nUKWggSyrrHBOBk
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -13,21 +17,28 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     {/* app providers, authProvider */}
-    <Provider store={store}>
-      {/* app providers, userProvider */}
-      {/* app providers, productProvider */}
-      {/* app providers, filterProvider */}
-      {/* app providers, cartProvider */}
-      {/* app providers, authWrapper */}
-      {/* i18next */}
-      <App />
-      {/* i18next */}
-      {/* app providers, authWrapper */}
-      {/* app providers, cartProvider */}
-      {/* app providers, filterProvider */}
-      {/* app providers, productProvider */}
-      {/* app providers, userProvider */}
-    </Provider>
+    <Auth0Provider
+      domain="dev-n7f-c1rx.us.auth0.com"
+      clientId="Kdv0f1TxqUbSMGqU4nUKWggSyrrHBOBk"
+      redirectUri={window.location.origin}
+      cacheLocation="localstorage"
+    >
+      <Provider store={store}>
+        {/* app providers, userProvider */}
+        {/* app providers, productProvider */}
+        {/* app providers, filterProvider */}
+        {/* app providers, cartProvider */}
+        {/* app providers, authWrapper */}
+        {/* i18next */}
+        <App />
+        {/* i18next */}
+        {/* app providers, authWrapper */}
+        {/* app providers, cartProvider */}
+        {/* app providers, filterProvider */}
+        {/* app providers, productProvider */}
+        {/* app providers, userProvider */}
+      </Provider>
+    </Auth0Provider>
     {/* app providers, authProvider */}
   </React.StrictMode>
 );
