1356f343f9598c8121a91f40b1b2a5d3d316677e
TatsuKenshi <rockinbg@gmail.com>
2023-02-25 21:06:56 +0100

    adds cartColumns and cartTotals setup

--- src/components/cart-columns/index.tsx
+++ src/components/cart-columns/index.tsx
@@ -1,7 +1,26 @@
-import React from "react";
-
 const CartColumns = () => {
-  return <div>CartColumns</div>;
+  return (
+    <section>
+      <div className="content" style={{ display: "flex" }}>
+        <h5 className="" style={{ marginRight: "10px" }}>
+          item
+        </h5>
+        <h5 className="" style={{ marginRight: "10px" }}>
+          price
+        </h5>
+        <h5 className="" style={{ marginRight: "10px" }}>
+          quantity
+        </h5>
+        <h5 className="" style={{ marginRight: "10px" }}>
+          subtotal
+        </h5>
+        <span className="" style={{ marginRight: "10px" }}>
+          remove
+        </span>
+      </div>
+      <hr />
+    </section>
+  );
 };
 
 export default CartColumns;
--- src/components/cart-totals/index.tsx
+++ src/components/cart-totals/index.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { useState } from "react";
+import { getTotalAmount, getShippingFee } from "../../app/reducers/cartSlice";
+
+// user slice imports go here
+
+import { useSelector, useDispatch } from "react-redux";
+import { AppDispatch } from "../../app/store";
+import { formatPrice } from "../../utils/helpers";
+import { Link } from "react-router-dom";
 
 const CartTotals = () => {
-  return <div>CartTotals</div>;
+  // mock user state
+  const [myUser, setMyUser] = useState<boolean>(false);
+
+  const totalAmount = useSelector(getTotalAmount);
+  const shippingFee = useSelector(getShippingFee);
+  const dispatch = useDispatch<AppDispatch>();
+
+  return (
+    <section>
+      <article>
+        <h5>
+          Subtotal : <span>{formatPrice(totalAmount)}</span>{" "}
+        </h5>
+
+        <p className="">
+          Shipping fee : <span>{formatPrice(shippingFee)}</span>
+        </p>
+
+        <hr className="" />
+
+        <h4 className="">
+          Order total : <span>{formatPrice(totalAmount + shippingFee)}</span>
+        </h4>
+      </article>
+
+      <button>
+        <Link to="/checkout" className="btn">
+          Go to checkout
+        </Link>
+      </button>
+    </section>
+  );
 };
 
 export default CartTotals;
