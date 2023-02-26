41b160634e6a28eceeb4699996ad693a8b8f5a58
TatsuKenshi <rockinbg@gmail.com>
2023-02-26 21:02:23 +0100

    adds functions folder with test function, netlify.toml, stripe checkout types to components/types.ts

--- src/components/types.ts
+++ src/components/types.ts
@@ -58,3 +58,39 @@ export type GridViewTypes = {
 export type ListViewTypes = {
   products: ProductType[];
 };
+
+// stripe types
+export interface StripeCheckoutStatic {
+  configure(options: StripeCheckoutOptions): StripeCheckoutHandler;
+}
+
+export interface StripeCheckoutHandler {
+  open(options?: StripeCheckoutOptions): void;
+  close(): void;
+}
+
+export interface StripeCheckoutOptions {
+  key?: string | undefined;
+  token?(token: stripe.Token): void;
+  source?(source: stripe.Source): void;
+  image?: string | undefined;
+  name?: string | undefined;
+  description?: string | undefined;
+  amount?: number | undefined;
+  locale?: string | undefined;
+  currency?: string | undefined;
+  panelLabel?: string | undefined;
+  zipCode?: boolean | undefined;
+  billingAddress?: boolean | undefined;
+  email?: string | undefined;
+  shippingAddress?: boolean | undefined;
+  label?: string | undefined;
+  allowRememberMe?: boolean | undefined;
+  bitcoin?: boolean | undefined;
+  alipay?: boolean | "auto" | undefined;
+  alipayReusable?: boolean | undefined;
+  opened?(): void;
+  closed?(): void;
+}
+
+export declare var StripeCheckout: StripeCheckoutStatic;
