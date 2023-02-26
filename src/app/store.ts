5809ad65e5f61566d31a555f153b6c17d854cdea
TatsuKenshi <rockinbg@gmail.com>
2023-02-26 02:26:00 +0100

    removes the userSlice from the rtk store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "./reducers/productsSlice";
-import userReducer from "./reducers/userSlice";
 import cartReducer from "./reducers/cartSlice";
 
 export const store = configureStore({
   reducer: {
     products: productsReducer,
-    user: userReducer,
     cart: cartReducer,
   },
 });
