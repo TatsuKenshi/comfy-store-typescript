892b434b58e495a437c2273ef00e167bc213802e
TatsuKenshi <rockinbg@gmail.com>
2023-06-09 23:56:52 +0200

    extends the number of faq questions

--- src/components/faq-questions-list/index.tsx
+++ src/components/faq-questions-list/index.tsx
@@ -6,6 +6,13 @@ const FaqQuestionsList = () => {
     question1: false,
     question2: false,
     question3: false,
+    question4: false,
+    question5: false,
+    question6: false,
+    question7: false,
+    question8: false,
+    question9: false,
+    question10: false,
   });
 
   const updateQuestions = (questionId: string, questionState: boolean) => {
@@ -102,6 +109,224 @@ const FaqQuestionsList = () => {
 
         {/* answer div */}
         {questions.question3 && (
+          <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias
+            tempore ipsum ab, mollitia, itaque nesciunt commodi obcaecati eos
+            iusto corrupti quod recusandae nobis voluptas voluptatem.
+          </div>
+        )}
+      </article>
+      {/* single question */}
+      <article>
+        {/* question div */}
+        <div>
+          <div>
+            <p>The question goes here. What is it?</p>
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                if (questions.question4) {
+                  updateQuestions("question4", false);
+                } else {
+                  updateQuestions("question4", true);
+                }
+              }}
+            >
+              {questions.question4 ? <FaChevronDown /> : <FaChevronUp />}
+            </button>
+          </div>
+        </div>
+
+        {/* answer div */}
+        {questions.question4 && (
+          <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias
+            tempore ipsum ab, mollitia, itaque nesciunt commodi obcaecati eos
+            iusto corrupti quod recusandae nobis voluptas voluptatem.
+          </div>
+        )}
+      </article>
+      {/* single question */}
+      <article>
+        {/* question div */}
+        <div>
+          <div>
+            <p>The question goes here. What is it?</p>
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                if (questions.question4) {
+                  updateQuestions("question4", false);
+                } else {
+                  updateQuestions("question4", true);
+                }
+              }}
+            >
+              {questions.question4 ? <FaChevronDown /> : <FaChevronUp />}
+            </button>
+          </div>
+        </div>
+
+        {/* answer div */}
+        {questions.question4 && (
+          <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias
+            tempore ipsum ab, mollitia, itaque nesciunt commodi obcaecati eos
+            iusto corrupti quod recusandae nobis voluptas voluptatem.
+          </div>
+        )}
+      </article>
+      {/* single question */}
+      <article>
+        {/* question div */}
+        <div>
+          <div>
+            <p>The question goes here. What is it?</p>
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                if (questions.question4) {
+                  updateQuestions("question4", false);
+                } else {
+                  updateQuestions("question4", true);
+                }
+              }}
+            >
+              {questions.question4 ? <FaChevronDown /> : <FaChevronUp />}
+            </button>
+          </div>
+        </div>
+
+        {/* answer div */}
+        {questions.question4 && (
+          <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias
+            tempore ipsum ab, mollitia, itaque nesciunt commodi obcaecati eos
+            iusto corrupti quod recusandae nobis voluptas voluptatem.
+          </div>
+        )}
+      </article>
+      {/* single question */}
+      <article>
+        {/* question div */}
+        <div>
+          <div>
+            <p>The question goes here. What is it?</p>
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                if (questions.question7) {
+                  updateQuestions("question7", false);
+                } else {
+                  updateQuestions("question7", true);
+                }
+              }}
+            >
+              {questions.question7 ? <FaChevronDown /> : <FaChevronUp />}
+            </button>
+          </div>
+        </div>
+
+        {/* answer div */}
+        {questions.question7 && (
+          <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias
+            tempore ipsum ab, mollitia, itaque nesciunt commodi obcaecati eos
+            iusto corrupti quod recusandae nobis voluptas voluptatem.
+          </div>
+        )}
+      </article>
+      {/* single question */}
+      <article>
+        {/* question div */}
+        <div>
+          <div>
+            <p>The question goes here. What is it?</p>
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                if (questions.question8) {
+                  updateQuestions("question8", false);
+                } else {
+                  updateQuestions("question8", true);
+                }
+              }}
+            >
+              {questions.question8 ? <FaChevronDown /> : <FaChevronUp />}
+            </button>
+          </div>
+        </div>
+
+        {/* answer div */}
+        {questions.question8 && (
+          <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias
+            tempore ipsum ab, mollitia, itaque nesciunt commodi obcaecati eos
+            iusto corrupti quod recusandae nobis voluptas voluptatem.
+          </div>
+        )}
+      </article>
+      {/* single question */}
+      <article>
+        {/* question div */}
+        <div>
+          <div>
+            <p>The question goes here. What is it?</p>
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                if (questions.question9) {
+                  updateQuestions("question9", false);
+                } else {
+                  updateQuestions("question9", true);
+                }
+              }}
+            >
+              {questions.question9 ? <FaChevronDown /> : <FaChevronUp />}
+            </button>
+          </div>
+        </div>
+
+        {/* answer div */}
+        {questions.question9 && (
+          <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias
+            tempore ipsum ab, mollitia, itaque nesciunt commodi obcaecati eos
+            iusto corrupti quod recusandae nobis voluptas voluptatem.
+          </div>
+        )}
+      </article>
+
+      {/* single question */}
+      <article>
+        {/* question div */}
+        <div>
+          <div>
+            <p>The question goes here. What is it?</p>
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                if (questions.question10) {
+                  updateQuestions("question10", false);
+                } else {
+                  updateQuestions("question10", true);
+                }
+              }}
+            >
+              {questions.question10 ? <FaChevronDown /> : <FaChevronUp />}
+            </button>
+          </div>
+        </div>
+
+        {/* answer div */}
+        {questions.question10 && (
           <div>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias
             tempore ipsum ab, mollitia, itaque nesciunt commodi obcaecati eos
